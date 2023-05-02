75e9077b3461ef913283296844c15394638ef443
bamtolee <shtjdgh00@gmail.com>
2023-05-02 17:54:34 +0900

    feat: 게시판 검색 기능 중 구군 목록 불러오기 구현 중
    Fixes: #4

--- src/main/java/com/ssafy/enjoytrip/controller/SearchAttractionController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/SearchAttractionController.java
@@ -1,8 +1,29 @@
 package com.ssafy.enjoytrip.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.enjoytrip.controller.request.GetGugunRequest;
+import com.ssafy.enjoytrip.service.SearchAttractionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/search")
 public class SearchAttractionController {
 
+    private final SearchAttractionService searchAttractionService;
+
+    public SearchAttractionController(SearchAttractionService searchAttractionService) {
+        this.searchAttractionService = searchAttractionService;
+    }
+
+    @GetMapping("/{sidoCode}")
+    public ResponseEntity<List<String>> getGugun(@RequestBody GetGugunRequest request) {
+        List<String> gugunCodeList = new ArrayList<>();
+        gugunCodeList = searchAttractionService.getGugun(request.GetGugun());
+        ResponseEntity<List<String>> re = new ResponseEntity<>(gugunCodeList, HttpStatus.OK);
+        return re;
+    }
 }
