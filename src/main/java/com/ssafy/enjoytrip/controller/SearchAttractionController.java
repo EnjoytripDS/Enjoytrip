e1831043907c904a34d8645df2489d32979c93bc
DESKTOP-DOGVPUB\SSAFY <ggg@gmail.com>
2023-05-03 14:31:21 +0900

    Feat: 게시판 검색 기능 중 구군 목록 불러오기 구현
    시도 선택 시 구군 목록 불러오기 구현. ResponseEntity로 리턴
    
    Resolves: #4

--- src/main/java/com/ssafy/enjoytrip/controller/SearchAttractionController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/SearchAttractionController.java
@@ -1,13 +1,11 @@
 package com.ssafy.enjoytrip.controller;
 
-import com.ssafy.enjoytrip.controller.request.GetGugunRequest;
 import com.ssafy.enjoytrip.service.SearchAttractionService;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +20,9 @@ public class SearchAttractionController {
     }
 
     @GetMapping("/{sidoCode}")
-    public ResponseEntity<List<String>> getGugun(@RequestBody GetGugunRequest request) {
-        List<String> gugunCodeList = new ArrayList<>();
-        gugunCodeList = searchAttractionService.getGugun(request.GetGugun());
-        ResponseEntity<List<String>> re = new ResponseEntity<>(gugunCodeList, HttpStatus.OK);
-        return re;
+    public ResponseEntity<List<String>> getGugun(@PathVariable("sidoCode") int sidoCode) {
+        List<String> gugunList = searchAttractionService.getGugun(sidoCode);
+
+        return new ResponseEntity<List<String>>(gugunList, HttpStatus.OK);
     }
 }
