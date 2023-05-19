62d8e5eac91993a6b965a5ac6fa3e6ebe9d6a1a7
@ydajeong7 <ydajeong7@gmail.com>
2023-05-19 16:08:41 +0900

    Feat: Q&A 게시판 기능 구현
    - 게시글 CRUD
    - 검색 기능

--- src/main/java/com/ssafy/enjoytrip/qnaboard/controller/QnaBoardController.java
+++ src/main/java/com/ssafy/enjoytrip/qnaboard/controller/QnaBoardController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/qna-board")
 public class QnaBoardController {
 
-    // TODO response 공통화
     private static final String SUCCESS = "succes";
     private static final String FAIL = "fail";
 
