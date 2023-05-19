2b7c7089439328efaa0719c9298628c5c822a7dc
@ydajeong7 <ydajeong7@gmail.com>
2023-05-19 16:13:30 +0900

    Refactor: CORS 설정 및 로그인 인터셉터 잠시 중지
    - frontend 통신을 위한 CORS 설정

--- src/main/java/com/ssafy/enjoytrip/commons/config/WebConfig.java
+++ src/main/java/com/ssafy/enjoytrip/commons/config/WebConfig.java
@@ -2,17 +2,23 @@ package com.ssafy.enjoytrip.commons.config;
 
 import com.ssafy.enjoytrip.commons.auth.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoginCheckInterceptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/**/user", "/**/user/login", "/**/user/check/*", "/**/user/logout");
+//    }
+
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/**/user", "/**/user/login", "/**/user/check/*", "/**/user/logout");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
