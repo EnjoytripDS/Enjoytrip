461e1d88333c131559e47f7c63ea9771a20956e1
@ydajeong7 <ydajeong7@gmail.com>
2023-05-19 16:20:02 +0900

    Style: code style 적용

--- src/main/java/com/ssafy/enjoytrip/commons/config/WebConfig.java
+++ src/main/java/com/ssafy/enjoytrip/commons/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.ssafy.enjoytrip.commons.config;
 
-import com.ssafy.enjoytrip.commons.auth.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
