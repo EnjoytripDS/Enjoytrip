cfe209265cbb3be7f1fad28f0aad192cb354c879
@ydajeong7 <ydajeong7@gmail.com>
2023-05-24 10:04:32 +0900

    Refactor: 구군 리스트 response 변경

--- src/main/java/com/ssafy/enjoytrip/attraction/service/AttractionService.java
+++ src/main/java/com/ssafy/enjoytrip/attraction/service/AttractionService.java
@@ -1,12 +1,13 @@
 package com.ssafy.enjoytrip.attraction.service;
 
 import com.ssafy.enjoytrip.attraction.dto.Attraction;
+import com.ssafy.enjoytrip.attraction.dto.Gugun;
 import com.ssafy.enjoytrip.attraction.dto.SearchCondition;
 import java.util.List;
 
 public interface AttractionService {
 
-    List<String> getGugun(int sidoCode);
+    List<Gugun> getGugun(int sidoCode);
 
     List<Attraction> search(SearchCondition searchCondition);
 
--- src/main/java/com/ssafy/enjoytrip/attraction/service/AttractionServiceImpl.java
+++ src/main/java/com/ssafy/enjoytrip/attraction/service/AttractionServiceImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.enjoytrip.attraction.service;
 
 import com.ssafy.enjoytrip.attraction.dao.AttractionDao;
 import com.ssafy.enjoytrip.attraction.dto.Attraction;
+import com.ssafy.enjoytrip.attraction.dto.Gugun;
 import com.ssafy.enjoytrip.attraction.dto.SearchCondition;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class AttractionServiceImpl implements AttractionService {
     }
 
     @Override
-    public List<String> getGugun(int sidoCode) {
+    public List<Gugun> getGugun(int sidoCode) {
         return attractionDao.getGugun(sidoCode);
     }
 
